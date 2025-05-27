39129857641d416db36226a9be74f17e7afca14d
ArslanShahbaz007 <ranaarslanshahbaz@gmail.com>
2025-05-27 09:10:49 -0700

    second commit

--- app/lib/sanity.ts
+++ app/lib/sanity.ts
@@ -1,8 +1,14 @@
 import {createClient } from "next-sanity";
-
+import  ImageUrlBuilder  from "@sanity/image-url";
 export const client = createClient({ 
     apiVersion: '2023-12-01',
     dataset: 'production',
     projectId: '9sb6j9a6',
     useCdn: false,
 })
+
+const builder = ImageUrlBuilder(client)
+
+export function urlFor(source: any) {
+    return builder.image(source);
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
 // import Image from "next/image";
-import { Card } from "@/components/ui/card";
+import { Card, CardContent } from "@/components/ui/card";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
 import { simpleBlogCard } from "./lib/interface";
 import {client} from "./lib/sanity";
+import {urlFor} from "./lib/sanity"
+import Image from "next/image";
 async function getData(){
   const query = `*[_type=='blog'] | order(_createdAt desc) {
   title,
@@ -18,15 +22,25 @@ export default async function Home() {
   const data: simpleBlogCard[] = await getData();
   console.log(data);
   return (
-   <div className="grid grid-cols-1 lg:grid-cols-4 mt-5">
-    {
-      data.map((post, idx)=>(
-        <Card key={idx}>
-        {/* <Image src/> */}
-        <Card/>
-))
-    }
-    
-   </div>
+   <div className="grid grid-cols-1 md:grid-cols-2 gap-5 mt-5">
+      {data.map((post, idx) => (
+        <Card key={idx} className="p-4">
+          <Image
+            src={urlFor(post.titleImage).url()}
+            alt={post.title}
+            height={500}
+            width={500}
+            className="rounded-md object-cover"
+          />
+      <CardContent className="mt-5">
+        <h2 className="text-lg line-clamp-2">{post.title}</h2>
+        <p className="line-clamp-3 text-sm mt-2 text-gray-600 dark:text-gray-300">{post.smallDescription}</p>
+        <Button asChild className="w-full mt-7">
+          <Link href={`/blog/${post.currentSlug}`}>Read More</Link>
+           </Button>
+      </CardContent>
+        </Card>
+      ))}
+    </div>
   );
 }
